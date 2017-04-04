1f97fa5ab1954d32378bd49decb18c84d7f35957
MARIE-ANDREE JACOB L'ITALIEN <m_jacobl@cda026ev5709x.local>
2017-04-04 13:46:37 -0400

    Making Loading Button

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Making the loading page, until everything is fully loaded. I might change the little icon later.
+$(document).ready(function() {
+
+  $(window).on("load", function () {
+    $('#loading').hide();
+  });
+
+});
